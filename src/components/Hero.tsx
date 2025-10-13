1191dc0bb6c4a7503ad409ce25c0b4a6b6e4b829
xiasudharsank-glitch <xiasudharsank@gmail.com>
2025-10-13 14:12:40 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,7 +46,11 @@ export default function Hero() {
         </div>
       </div>
 
-     
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="w-6 h-10 border-2 border-slate-400 rounded-full flex justify-center">
+          <div className="w-1 h-3 bg-slate-400 rounded-full mt-2"></div>
+        </div>
+      </div>
     </section>
   );
 }
